a18fedffb837a35e1f5cad2267acf647e5fa8e35
ClodaghMM <13559048@student.uts.edu.au>
2021-08-25 16:46:58 +1000

    70% band

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -6,11 +6,15 @@ public class LoadAssets : MonoBehaviour
 {
 
     public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //game object should be created here
+        Instantiate(redObj,new Vector3(2.0F,0.0F,0.0F), Quaternion.identity);
+
+        Instantiate(blueObj, new Vector3(-2.0F,0.0F,0.0F), Quaternion.identity);
     }
 
     // Update is called once per frame
