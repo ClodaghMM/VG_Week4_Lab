a874971e7a921d6cff8a9c34a9697e3132cde11c
ClodaghMM <13559048@student.uts.edu.au>
2021-08-26 10:30:22 +1000

    80% Band

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    int i = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //increment i
+        ++i; 
+        Debug.Log(gameObject.name);
+        
+    }
+}
