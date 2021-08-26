6ed9bf385bdb9c216eea002c250dbd3b146999be
ClodaghMM <13559048@student.uts.edu.au>
2021-08-26 11:57:05 +1000

    100%git add . git add .

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PrintAndHide : MonoBehaviour
+
 {
     int i = 3;
     public Renderer rend;
+    int rInt; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rInt = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -18,18 +21,16 @@ public class PrintAndHide : MonoBehaviour
         //increment i
         ++i;
         Debug.Log(gameObject.name +  ":" + i);
-        
-        int rInt = Random.Range(0, 251);
-
         if (gameObject.tag == 
         "Red" && i == 100) 
         {
             gameObject.SetActive(false);
         }
 
-        if (gameObject.tag == "Blue" && rInt > 200 && rInt < 251)
+        if (gameObject.tag == "Blue" && i == rInt)
         {
-            rend = gameObject.GetComponent<Renderer>();
+            rend = gameObject.
+            GetComponent<Renderer>();
             rend.enabled = false;
         } 
 
